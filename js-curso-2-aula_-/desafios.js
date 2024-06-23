4e8eb7a08c843883634689947281caca102bea86
oCastelo <gustavocastelobf@gmail.com>
2024-06-23 17:54:23 +0100

    projeto inicial

---
+++ js-curso-2-aula_-/desafios.js
@@ -0,0 +1,17 @@
+let listaGenerica = []
+
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python']
+
+linguagensDeProgramacao.push ('java', 'Ruby', 'GoLang')
+
+
+let listaDeNomes = ['Rute', 'Jojo', 'Rael']
+console.log (listaDeNomes[0])
+
+let listaDeNomes2 = ['Esmerina', 'Renato', 'Raphael']
+console.log (listaDeNomes2[1])
+
+let listaDeNomes3 = ['Pedro', 'Boris', 'Fátima']
+console.log (listaDeNomes3[listaDeNomes3.length - 1])
+
+console.log(linguagensDeProgramacao)
